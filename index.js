9fb446a26c4809188c527c6f95ab1da04278942c
Steffen O <steffenosterburg@googlemail.com>
2018-07-06 11:45:03 +0200

    removed magick command for backwards compatibility

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ var child;
 
 let im = require("./lib/imagick");
 
-
-child = exec("magick -version", function (error, stdout, stderr) {
+child = exec("convert -version", function (error, stdout, stderr) {
     if (error !== null) {
         console.log('imagemagick is not installed! Please install imagemagick from https://www.imagemagick.org/');
         process.exit(1);
     }
 });
 
+console.log('exporting module');
 module.exports = im;
+console.log('done');
